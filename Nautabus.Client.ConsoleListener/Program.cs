0a92282ee9f0b7e5652820593c241016be205847
Stephen Redd <redds@reddnet.net>
2015-07-12 15:23:48 -0400

    working strong typed signalr based hub test

--- Nautabus.Client.ConsoleListener/Program.cs
+++ Nautabus.Client.ConsoleListener/Program.cs
@@ -9,20 +9,56 @@ namespace Nautabus.Client.ConsoleListener
 {
     class Program
     {
+        static IHubProxy Nautaproxy { get; set; }
+
         static void Main(string[] args)
         {
             ConnectToHub().Wait();
+
+            var testMessage = new SampleMessage {Id = 55, MessageName = "test", MessageContent = "this is a test message of Sample message type from client"};
+
+            Nautaproxy.Invoke("Publish", "sample-messages", testMessage);
+
+
             Console.ReadLine();
         }
 
         public static async Task ConnectToHub()
         {
-            var hubConnection = new HubConnection("http://localhost:9000/",new Dictionary<string, string> { {"clientName","ConsoleListener"}});
-            IHubProxy myHubProxy = hubConnection.CreateHubProxy("MyHub");
-            myHubProxy.On<string,string>("addMessage", (name,message )=> Console.WriteLine("{0} {1}", name, message));
+            var hubConnection = new HubConnection("http://localhost:3411/", new Dictionary<string, string> { { "clientName", "ConsoleListener" } });
+            Nautaproxy = hubConnection.CreateHubProxy("Nautahub");
+
+            
+            Nautaproxy.On<SampleMessage>("sample-messages", smsg =>
+            {
+                Console.WriteLine("message received for event 'sample-messages'");
+                Console.WriteLine("message id = " + smsg.Id);
+                Console.WriteLine("message name = " + smsg.MessageName);
+                Console.WriteLine("message content = " + smsg.MessageContent);
+                Console.WriteLine("---------------------------");
+            });
+
+            Nautaproxy.On<string>("string-messages", s =>
+            {
+                Console.WriteLine("message received for event 'string-messages'");
+                Console.WriteLine("message = " + s);
+                Console.WriteLine("---------------------------");
+            });
             await hubConnection.Start();
+
+
+            await Nautaproxy.Invoke("subscribe", "sample-messages");
+            
         }
 
 
+    }
+
+
+    public class SampleMessage
+    {
+        public int Id { get; set; }
+        public string MessageName { get; set; }
+        public string MessageContent { get; set; }
     }
 }
---
+++ Nautabus.Server/Nautahub.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Nautabus.Server
+{
+    public class Nautahub : Hub
+    {
+        public override Task OnConnected()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                System.Threading.Thread.Sleep(5000);
+                var hub = new EventHub();
+                var testMessage = new SampleMessage { Id = 55, MessageName = "test", MessageContent = "this is a test message like a Sample message from the server" };
+
+                hub.PublishMessage("sample-messages", testMessage);
+                hub.PublishMessage("string-messages", "this is the other event from the server");
+
+            }).ConfigureAwait(false);
+
+            //assuming your client included a clientName querystring param
+            //log message somewhere : Log("Connection from " + Context.QueryString["clientName"]));
+            return base.OnConnected();
+        }
+
+        public void Subscribe(string topic)
+        {
+            Groups.Add(Context.ConnectionId, topic);
+        }
+        public void Unsubscribe(string topic)
+        {
+            Groups.Remove(Context.ConnectionId, topic);
+        }
+
+        public void Publish(string topic, SampleMessage message)
+        {
+            var eh = new EventHub();
+            eh.PublishMessage(topic, message);
+        }
+
+
+    }
+
+    public class SampleMessage
+    {
+        public int Id { get; set; }
+        public string MessageName { get; set; }
+        public string MessageContent { get; set; }
+    }
+
+    public class EventHub
+    {
+        public void PublishMessage<T>(string topic, T message)
+        {
+            var hub = GlobalHost.ConnectionManager.GetHubContext<Nautahub>();
+            ((IClientProxy)hub.Clients.Group(topic)).Invoke(topic, message).Wait();
+        }
+    }
+
+
+}
